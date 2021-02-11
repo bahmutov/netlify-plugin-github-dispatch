086259887c98502bfe8aec7c798949bbf2a4777f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-11 12:07:07 -0500

    update

--- src/dispatch.js
+++ src/dispatch.js
@@ -1,5 +1,11 @@
 const { Octokit } = require('@octokit/rest')
 
+/**
+ * Triggers workflow dispatch event in the given repository.
+ * Note: you must run the workflow at least once before it is found
+ * and can be triggered by the event.
+ * @param {} param0
+ */
 const dispatchWorkflow = async ({ auth }) => {
   const octokit = new Octokit({
     auth,
@@ -11,9 +17,15 @@ const dispatchWorkflow = async ({ auth }) => {
     owner,
     repo,
   })
-  console.log(workflows)
+  if (!workflows.data) {
+    throw new Error('Cannot find workflows data')
+  }
+  if (!workflows.data.total_count) {
+    throw new Error(`Could not find any workflows in ${owner}/${repo}`)
+  }
+  console.log(workflows.data.workflows)
 
-  const workflow_id = 'e2e'
+  const workflow_id = '.github/workflows/e2e.yml'
   const ref = process.env.BRANCH || 'main'
   console.log('triggering GitHub with %o', {
     owner,
