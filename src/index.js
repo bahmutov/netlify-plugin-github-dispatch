a2fd7fc4f6d4dff004c403477cf0a4e8437a35d1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-11 11:40:39 -0500

    try trigger build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+const { Octokit } = require('@octokit/rest')
 const debug = require('debug')('netlify-plugin-github-dispatch')
 
 module.exports = {
-  onSuccess: async ({ constants }) => {
+  onSuccess: async ({ constants, utils }) => {
     const isLocal = constants.IS_LOCAL
     const siteName = process.env.SITE_NAME
     const deployPrimeUrl = process.env.DEPLOY_PRIME_URL
@@ -11,5 +12,36 @@ module.exports = {
       deployPrimeUrl,
     })
     console.log('Deployed URL %s', deployPrimeUrl)
+
+    // if this plugin is missing something, report and error
+    // https://docs.netlify.com/configure-builds/build-plugins/create-plugins/#error-reporting
+    const auth = process.env.GITHUB_TOKEN
+    if (!auth) {
+      return utils.build.failPlugin('Missing env variable GITHUB_TOKEN')
+    }
+
+    try {
+      const octokit = new Octokit({
+        auth,
+      })
+      const owner = 'bahmutov'
+      const repo = 'netlify-plugin-github-dispatch'
+      const workflow_id = '.github/workflows/e2e.yml'
+      const ref = process.env.BRANCH
+      console.log('triggering GitHub with %o', {
+        owner,
+        repo,
+        workflow_id,
+        ref,
+      })
+      await octokit.actions.createWorkflowDispatch({
+        owner,
+        repo,
+        workflow_id,
+        ref,
+      })
+    } catch (error) {
+      return utils.build.failPlugin(err.message, { error })
+    }
   },
 }
